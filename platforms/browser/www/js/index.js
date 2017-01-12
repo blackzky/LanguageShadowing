3d0b1729a889ef925a6352683c0b7f7d294e7a03
Gervel Giva <gervelgiva@gmail.com>
2017-01-12 22:23:24 +0800

    Added support for mutiple voice models

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,39 +1,28 @@
-var S;
 $(function() {
     var TIMER;
     var CURRENT = 0;
     var LAST_VALUE = 0;
     var PAUSED = false;
 
-    var sound = new Howl({
-        src: ['audio/speech.m4a'],
-        sprite: {
-            all: [0, 246000],
-            id1: [2600, 1700],
-            id2: [4400, 3700],
-            id3: [8300, 1900],
-            id4: [10700, 3900],
-            id5: [15000, 4500]
-        }
-    });
-
-    S = sound; // DEBUG
+    var OVERRIDES = {};
+    var SOUND;
 
     // Events
+    loadVoiceModel($('#voiceModel').val());
+
     $('.kanji').tooltip({ placement: "top" });
     $('body').on('click', '#play', playAll);
     $('body').on('click', '#pause', pause);
     $('body').on('click', '#stop', stop);
     $('body').on('click', '.play', play);
-
+    $('#voiceModel').on('change', changeVoiceModel);
 
     // Functions
     function play(e) {
         e.preventDefault();
         stop(e);
         var id = $(this).data('id').trim();
-        sound.play(id);
-        console.log(`Playing: ${id}`);
+        SOUND.play(id);
 
         PAUSED = false;
         CURRENT = 0;
@@ -46,10 +35,10 @@ $(function() {
         if (PAUSED === true) {
             var pos = $('#time').val();
             console.log(`Play starting @ ${pos}`);
-            sound.seek(pos).play();
+            SOUND.seek(pos).play();
         } else {
             console.log('Play all');
-            sound.play("all");
+            SOUND.play("all");
         }
         PAUSED = false;
         TIMER = setInterval(iterateTimer, 1);
@@ -57,25 +46,48 @@ $(function() {
 
     function stop(e) {
         e.preventDefault();
-        sound.stop();
+        SOUND.stop();
         clearInterval(TIMER);
         $('#time').val(0);
     }
 
     function pause(e) {
         e.preventDefault();
-        sound.pause();
+        SOUND.pause();
         PAUSED = true;
         clearInterval(TIMER);
     }
 
     function iterateTimer() {
         if (PAUSED === false) {
-            LAST_VALUE = sound.seek();
+            LAST_VALUE = SOUND.seek();
             if (LAST_VALUE > CURRENT) {
-                CURRENT = sound.seek();
+                CURRENT = SOUND.seek();
                 $('#time').val(CURRENT);
             }
         }
+    }
+
+    function changeVoiceModel(e) {
+        console.log('loading: ' + this.value);
+        $('.btn').prop('disabled', 'disabled');
+        loadVoiceModel(this.value);
+    }
+
+    function loadVoiceModel(voiceModel) {
+        $.getJSON('data/' + voiceModel + '.json', function(data) {
+            OVERRIDES = data.overrides;
+
+            SOUND = new Howl({
+                src: data.src,
+                sprite: data.sprite,
+                onend: function() {
+                    clearInterval(TIMER);
+                },
+                onload: function() {
+                    $('.btn').prop('disabled', false);
+                }
+            });
+        });
     }
 });
